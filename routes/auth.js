328833c8a9f4697f4160b594c8dd71b56071799c
olanetsoft <heedris2olubisi@gmail.com>
2020-08-25 21:03:51 +0100

    home, login, sign in UI completed

--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,12 @@ router.get('/', (req, res)=>{
     res.render('index')
 });
 
+router.get('/login', (req, res)=>{
+    res.render('auth/login')
+});
+
+router.get('/register', (req, res)=>{
+    res.render('auth/register')
+});
+
 module.exports = router;
