1e015a7a34442890c3554d24ef07a1a760cb5454
olanetsoft <heedris2olubisi@gmail.com>
2020-08-25 17:06:54 +0100

    server setup

---
+++ app.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+const cookieSession = require('cookie-session');
+const flash = require("connect-flash");
+const csrf = require('csurf');
+
+const csrfProtection = csrf();
+
+// requiring the cookie parser
+const cookieParser = require('cookie-parser');
+
+
+
+
+const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 24 * 60 * 60 * 1000,
+        name: 'session',
+        keys: [process.env.SESSION_COOKIEKEY],
+    }),
+);
+// Cookie Parser
+app.use(cookieParser());
+app.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
+
+
+// setting the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Set flash response for feedback
+app.use(flash());
+
+
+// registering a middleware for server static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+// Global Middleware registered
+// Using morgan only in development
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
+
+// Middleware registered
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+app.use(cookieParser());
+
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use(compression());
+
+// Protecting the application against cyber attack
+
+app.use(csrfProtection);
+app.use((req, res, next) => {
+    const token = req.csrfToken();
+    res.cookie('csrf-token', token);
+    res.locals.csrfToken = req.csrfToken();
+    res.locals.currentUser = req.session.user;
+    next();
+});
+
+
+// ************ REGISTER ROUTES HERE ********** //
+
+
+// ************ END ROUTE REGISTRATION ********** //
+
+module.exports = app;
